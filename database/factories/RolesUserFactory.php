27ecf962d02f70c804da4a830c1175711305a8c6
IvanVillalozPina <al222010248@gmail.com>
2021-12-06 15:53:36 -0600

    Migraciones

---
+++ database/factories/RolesUserFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\roles_user;
+use Faker\Generator as Faker;
+
+$factory->define(roles_user::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
